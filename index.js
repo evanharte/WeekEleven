f2b17d10b73c142c2fd10bb764b87d0f0118d109
prawstho <peter.rawsthorne@keyin.com>
2022-11-21 15:30:46 -0330

    Prepare project from last weeks code base

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+global.DEBUG = true;
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true, })); // This is important!
+// app.use(methodOverride('_method')); // So is this!
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Cassian Andor'});
+});
+app.get('/about', (request, response) => {
+    response.render('about.ejs');
+});
+
+const actorsRouter = require('./routes/actors')
+app.use('/actors', actorsRouter);
+
+const mealsRouter = require('./routes/meals')
+app.use('/meals', mealsRouter);
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
+
+app.listen(PORT, () => {
+    console.log(`Simple app running on port ${PORT}.`)
+});
