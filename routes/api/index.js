546feaa719cf9e31609182b1d705fcb1fbf2e855
Peter <113168584+prawstho@users.noreply.github.com>
2023-03-22 11:30:12 -0230

    lecture changes complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ var router = require('express').Router();
 if(DEBUG) {
     console.log('ROUTE: /api/actors');
 }
-
+// http://localhost:3000/api/actors/
 const actorsRouter = require('./actors')
 router.use('/actors', actorsRouter);
 
