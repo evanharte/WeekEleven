4476783ed656b431cfa8fa892bf53334ea69171e
Peter <113168584+prawstho@users.noreply.github.com>
2023-08-01 15:46:03 -0230

    fulltext for both mongo and postgresql

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 if(DEBUG) {
     console.log('ROUTE: /api/actors');
     console.log('ROUTE: /api/logins');
+    console.log('ROUTE: /api/full');
 }
 // http://localhost:3000/api/actors/
 const actorsRouter = require('./actors')
@@ -11,5 +12,9 @@ router.use('/actors', actorsRouter);
 // http://localhost:3000/api/logins/
 const loginsRouter = require('./logins')
 router.use('/logins', loginsRouter);
+
+// http://localhost:3000/api/full/
+const fulltextRouter = require('./fulltext')
+router.use('/full', fulltextRouter);
 
 module.exports = router;
