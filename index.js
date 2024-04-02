c6044b60717f6663b165525ac6b8538ad8431878
Evan <evan.harte@keyin.com>
2024-04-02 18:04:02 -0230

    added working(!!!) search functionality

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const express = require('express');
-const methodOverride = require('method-override');
+const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 const PORT = 3000;
 
 global.DEBUG = true;
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true, })); // This is important!
-app.use(methodOverride('_method')); // So is this!
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false })); // This is important!
+app.use(methodOverride("_method")); // So is this!
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Cassian Andor'});
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "Cassian Andor" });
 });
-app.get('/about', (request, response) => {
-    response.render('about.ejs');
+app.get("/about", (request, response) => {
+  response.render("about.ejs");
 });
+// app.get("/search", (request, response) => {
+//   response.render("search.ejs");
+// });
 
-const actorsRouter = require('./routes/actors')
-app.use('/actors', actorsRouter);
+const actorsRouter = require("./routes/actors");
+app.use("/actors", actorsRouter);
 
-const loginsRouter = require('./routes/logins')
-app.use('/logins', loginsRouter);
+const loginsRouter = require("./routes/logins");
+app.use("/logins", loginsRouter);
 
-const usersRouter = require('./routes/users')
-app.use('/users', usersRouter);
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
 // anything beginning with "/api" will go into this
-const apiRouter = require('./routes/api')
-app.use('/api', apiRouter);
+const apiRouter = require("./routes/api");
+app.use("/api", apiRouter);
+
+// anything beginning with "/search" will go into this
+const searchRouter = require("./routes/search");
+app.use("/search", searchRouter);
 
 app.use((req, res) => {
-    res.status(404).render('404');
+  res.status(404).render("404");
 });
 
 app.listen(PORT, () => {
-    console.log(`Simple app running on port ${PORT}.`)
+  console.log(`Simple app running on port ${PORT}.`);
 });
